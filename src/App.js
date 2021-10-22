0f3f6ba8591675e3e7397cd10bc177b85f3c1f6c
Lucas Dreher <lucasdreher@gmail.com>
2021-10-22 20:02:31 +0200

    Moving data from form to the root App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
+	const addExpenseHandler = (expense) => {
+		console.log('inApp', expense);
+	};
+
 	const expenses = [
 		{
 			id: 'e1',
@@ -25,7 +30,7 @@ const App = () => {
 
 	return (
 		<div>
-			<h2>Let's get started!</h2>
+			<NewExpense onAddExpense={addExpenseHandler} />
 			<Expenses data={expenses} />
 		</div>
 	);
