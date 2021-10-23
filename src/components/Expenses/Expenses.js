0aebade78db439486e4cbc4940212826e55c5b87
Lucas Dreher <lucasdreher@gmail.com>
2021-10-23 10:11:59 +0200

    Filter expenses added Expenses.js controls the filter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,8 @@
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
 import './Expenses.css';
+import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
+import React, { useState } from 'react';
 
 function Expenses(props) {
 	const expenses = props.data;
@@ -8,7 +10,18 @@ function Expenses(props) {
 		<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
 	));
 
-	return <Card className="expenses">{expensesList}</Card>;
+	const [ filteredYear, setFilteredYear ] = useState('2020');
+
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
+	return (
+		<Card className="expenses">
+			<ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+			{expensesList}
+		</Card>
+	);
 }
 
 export default Expenses;
